7a45479ccd6a9766a5ae04dbe364d9bbfa47bf64
douglas-augusto <douglas-augusto@hotmail.com>
2018-04-24 11:28:31 -0300

    Condutas Activity 1 = Select Age

--- app/src/main/java/com/example/aluno/condutascancermama/MainActivity.java
+++ app/src/main/java/com/example/aluno/condutascancermama/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.aluno.condutascancermama;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private CardView botaoCondutas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoCondutas = (CardView) findViewById(R.id.condutasId);
+
+        botaoCondutas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Condutas.class);
+                startActivity(intent);
+            }
+        });
     }
 }
