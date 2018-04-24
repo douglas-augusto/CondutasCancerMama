5ae0f7da9e2ea695f6278225904497b115214863
douglas-augusto <douglas-augusto@hotmail.com>
2018-04-24 15:39:06 -0300

    Condutas Activity 40 more.

--- app/src/main/java/com/example/aluno/condutascancermama/Condutas.java
+++ app/src/main/java/com/example/aluno/condutascancermama/Condutas.java
@@ -1,13 +1,28 @@
 package com.example.aluno.condutascancermama;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class Condutas extends AppCompatActivity {
+
+    private CardView botaoMaisQuarenta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_condutas);
+
+        botaoMaisQuarenta = (CardView)findViewById(R.id.botaoMais40);
+
+        botaoMaisQuarenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Condutas.this, CondutasMais40.class);
+                startActivity(intent);
+            }
+        });
     }
 }
