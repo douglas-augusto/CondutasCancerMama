8f3bfb822a99d0ec31fbc3e0b764a98f1c617016
douglas-augusto <douglas-augusto@hotmail.com>
2018-04-24 21:33:31 -0300

    Condutas +40 e -40 quase prontas

--- app/src/main/java/com/example/aluno/condutascancermama/Condutas.java
+++ app/src/main/java/com/example/aluno/condutascancermama/Condutas.java
@@ -9,18 +9,30 @@ import android.view.View;
 public class Condutas extends AppCompatActivity {
 
     private CardView botaoMaisQuarenta;
+    private CardView botaoMenosQuarenta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_condutas);
 
+        this.setTitle("Selecione a idade da paciente");
+
         botaoMaisQuarenta = (CardView)findViewById(R.id.botaoMais40);
+        botaoMenosQuarenta = (CardView)findViewById(R.id.botaoMenos40);
 
         botaoMaisQuarenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Condutas.this, CondutasMais40.class);
+                startActivity(intent);
+            }
+        });
+
+        botaoMenosQuarenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Condutas.this, CondutasMenos40.class);
                 startActivity(intent);
             }
         });
